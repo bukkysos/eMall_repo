3da0d93f197ff432abe1ebae079003d279d8847c
chinonso williams <williamschinonso11@gmail.com>
2018-11-27 13:13:15 +0100

    created mongoose connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,13 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/eMall', {useNewUrlParser: true});
 
 // error handler
 app.use(function(err, req, res, next) {
