b7c01a7b8f5a7c416584461d0082efa3a74f52d8
chinonso williams <williamschinonso11@gmail.com>
2018-11-28 19:33:08 +0100

    updated UserController

---
+++ controllers/UserController.js
@@ -0,0 +1,24 @@
+var model = require('../models/User');
+var service = require('../services/UserService');
+
+exports.addUser = function(req, res){
+    var data = {
+        name : req.body.name,
+        email : req.body.email,
+        password: req.body.password
+    }
+    return service.addUser(req, res, data);
+}
+
+exports.updateUser = function(req, res){
+    var id = req.params.id;
+    var options = req.body
+};
+
+exports.deleteUser = function(req, res){
+    var data = {_id:req.params.id}
+}
+
+exports.getUserByParam = function(req, res){
+    var options = req.query
+}
