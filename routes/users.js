92b7f83142ba09d39226e11e695660eff80411fa
chinonso williams <williamschinonso11@gmail.com>
2018-11-29 14:37:05 +0100

    created updateUser router

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ var userController = require('../controllers/UserController');
 /* GET users listing. */
 router.post('/create', userController.addUser);
 router.get('/', userController.getUsers);
+router.post('/update/:id', userController.updateUser);
 
 module.exports = router;
