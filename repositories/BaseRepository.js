8ef2ca6eb36d4c76d81dad967e2b3d497b429b3f
chinonso williams <williamschinonso11@gmail.com>
2018-11-29 13:02:29 +0100

    created getAll Function

--- repositories/BaseRepository.js
+++ repositories/BaseRepository.js
@@ -7,6 +7,10 @@ BaseRepository.prototype.add = function(data, callback){
     this.model.create(data, callback);
 }
 
+BaseRepository.prototype.getAll = function(options, columns, callback){
+    this.model.find(options, columns, callback);
+}
+
 module.exports = function(model){
     return new BaseRepository(model);
 }
