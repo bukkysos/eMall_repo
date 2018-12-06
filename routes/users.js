3dce702cd15c239049efb1326310d80691c79211
chinonso williams <williamschinonso11@gmail.com>
2018-12-06 18:01:36 +0100

    created login route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var userController = require('../controllers/UserController');
 
 /* GET users listing. */
 router.post('/create', userController.addUser);
+router.post('/login', userController.loginUser);
 router.get('/', userController.getUsers);
 router.post('/update/:id', userController.updateUser);
 router.delete('/delete/:id', userController.deleteUser);
