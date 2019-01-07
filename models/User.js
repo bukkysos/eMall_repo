fc521ba7cee06902df2d94c3205599f7871c2b55
chinonso williams <williamschinonso11@gmail.com>
2019-01-08 00:24:19 +0100

    edited userSchema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,12 @@ var userSchema = mongoose.Schema({
     username : String,
     email : {type: String, unique: true},
     password : String,
+    // title: String,
+    // description : String,
+    // quantity: Number,
+    // image: String,
+    // item : String
+    // profile: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
     });
 
 module.exports = mongoose.model('User', userSchema);
